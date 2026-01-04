df11e713c831cf8281be5d06db0cb7ff286fbbe3
Sergey Zhukov <szhukov@aligntech.com>
2026-01-04 10:53:02 +0300

    refactor: simplify aws errors

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,6 @@
 use std::io::Error as IoError;
 
 use aws_sdk_s3::error::SdkError;
-use aws_sdk_s3::operation::complete_multipart_upload::CompleteMultipartUploadError;
-use aws_sdk_s3::operation::create_multipart_upload::CreateMultipartUploadError;
-use aws_sdk_s3::operation::get_object::GetObjectError;
-use aws_sdk_s3::operation::list_objects_v2::ListObjectsV2Error;
-use aws_sdk_s3::operation::put_object::PutObjectError;
-use aws_sdk_s3::operation::upload_part::UploadPartError;
 use aws_smithy_types::byte_stream::error::Error as AWSSmithyError;
 use color_eyre::eyre::Report;
 use thiserror::Error;
@@ -14,26 +8,11 @@ use url::ParseError;
 
 #[derive(Debug, Error)]
 pub enum UtilsError {
+    #[error("AWS S3 error")]
+    AwsS3(#[source] Box<dyn std::error::Error + Send + Sync>),
+
     #[error("IO error")]
     IoError(#[from] IoError),
-
-    #[error("AWS GetObjectError error")]
-    GetObjectError(#[from] SdkError<GetObjectError>),
-
-    #[error("AWS ListObjectsV2Error error")]
-    ListObjectsV2Error(#[from] SdkError<ListObjectsV2Error>),
-
-    #[error("AWS CreateMultipartUploadError error")]
-    CreateMultipartUploadError(#[from] SdkError<CreateMultipartUploadError>),
-
-    #[error("AWS CompleteMultipartUploadError error")]
-    CompleteMultipartUploadError(#[from] SdkError<CompleteMultipartUploadError>),
-
-    #[error("AWS PutObjectError error")]
-    PutObjectError(#[from] SdkError<PutObjectError>),
-
-    #[error("AWS UploadPartError error")]
-    UploadPartError(#[from] SdkError<UploadPartError>),
 
     #[error("AWSSmithy error")]
     AWSSmithyError(#[from] AWSSmithyError),
@@ -46,4 +25,13 @@ pub enum UtilsError {
 
     #[error("Unexpected error")]
     UnexpectedError(#[source] Report),
+}
+
+impl<E> From<SdkError<E>> for UtilsError
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    fn from(err: SdkError<E>) -> Self {
+        UtilsError::AwsS3(Box::new(err))
+    }
 }
